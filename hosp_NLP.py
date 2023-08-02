11af457a075b4081885723caf22f83abd18e5268
Evan Anderson <60617161+evan1417@users.noreply.github.com>
2023-08-01 20:46:47 -0400

    it makes a shit graph now
    created a node network that would take the 16 sites and output the icd categories with the main three topic labels from data set. This would then be sorted by value for the topic set to see associations between patient age, acuity of icd category, ang length of stay

--- hosp_NLP.py
+++ hosp_NLP.py
@@ -1,30 +1,74 @@
 import pandas as pd
+import networkx as nx
 import matplotlib.pyplot as plt
 
-def process_csv(input_file):
-    # Read the CSV file into a DataFrame
-    df = pd.read_csv(input_file)
+# Replace 'your_file_path' with the actual file path of the CSV file
+file_path = r'C:\Users\Evan Anderson\Desktop\data_with_NLP_sorted.csv'
+data = pd.read_csv(file_path)
 
-    # Group data by 'site' and 'Topic Label' to count occurrences
-    grouped_data = df.groupby(['site', 'Topic Label']).size().reset_index(name='Frequency')
+# Calculate the average 'compound' for each descriptor listed under 'topic label'
+average_compound_by_topic = data.groupby('topic label')['compound'].mean().reset_index()
 
-    # Pivot the DataFrame to get 'site' as rows, 'Topic Label' as columns, and 'Frequency' as values
-    pivot_table = grouped_data.pivot(index='site', columns='Topic Label', values='Frequency')
-    pivot_table = pivot_table.fillna(0)  # Replace NaN with 0 if some combinations are missing
+# Create a graph
+G = nx.Graph()
 
-    #testing data to create a bar plot per hospital
-    # Visualize the data using a bar plot
-    #outputs simple linear
-    plt.figure(figsize=(12, 6))
-    pivot_table.plot(kind='bar', stacked=True, colormap='viridis')
-    plt.xlabel('Site')
-    plt.ylabel('Frequency')
-    plt.title('Frequency of Topic Labels per Site')
-    plt.legend(title='Topic Label')
-    plt.grid(axis='y', linestyle='--', alpha=0.7)
-    plt.tight_layout()
-    plt.show()
+# Add nodes for the 'site' variables
+sites = data['site'].unique()
+for site in sites:
+    G.add_node(site)
 
-if __name__ == "__main__":
-    # Replace 'input_file.csv' with the path to your actual CSV file
-    process_csv(r'C:\\Users\Evan Anderson\Desktop\data_with_NLP_sorted.csv')
+# Add nodes for the average compound of each descriptor under 'topic label'
+for _, row in average_compound_by_topic.iterrows():
+    topic_label = row['topic label']
+    G.add_node(topic_label)
+
+# Add edges between 'site' and average compound nodes
+for _, row in data.iterrows():
+    site = row['site']
+    topic_label = row['topic label']
+    compound = row['compound']
+
+    G.add_edge(site, topic_label, weight=compound)
+
+# Convert 'acuity' column to numeric type
+data['acuity'] = pd.to_numeric(data['acuity'], errors='coerce')
+
+# Create a stored dataset for 'icd.category' and 'acuity'
+icd_acuity_data = data[data['acuity'] <= 3][['icd.category', 'acuity', 'topic label']]
+
+# Add edges between 'icd.category' and average compound nodes (if acuity is <= 3)
+for _, row in icd_acuity_data.iterrows():
+    icd_category = row['icd.category']
+    topic_label = row['topic label']
+    compound = data[data['topic label'] == topic_label]['compound'].mean()
+
+    G.add_edge(icd_category, topic_label, weight=compound, thickness='bold')
+
+# Add edges between 'los.min', 'patient.age', and average compound nodes
+for _, row in data.iterrows():
+    los_min = row['los.min']
+    if los_min == '*':  # Ignore non-numeric values
+        continue
+    los_min = float(los_min)
+    patient_age = row['patient.age']
+    topic_label = row['topic label']
+    compound = row['compound']
+
+    if los_min > 120:
+        G.add_edge('los.min > 120', topic_label, weight=compound, thickness='bold')
+
+# Create a figure and axis for the plot
+plt.figure(figsize=(10, 8))
+ax = plt.gca()
+
+# Draw nodes and edges
+pos = nx.spring_layout(G, seed=42)  # Adjust the layout algorithm as needed
+nx.draw(G, pos, with_labels=True, node_size=2000, node_color='skyblue', font_size=8, font_weight='bold', font_color='black', ax=ax)
+nx.draw_networkx_edges(G, pos, width=[1.5 if 'thickness' in G.edges[edge] else 1.0 for edge in G.edges()], ax=ax)
+
+# Add edge labels with compound values
+edge_labels = {(u, v): f"{G[u][v]['weight']:.2f}" for u, v in G.edges() if 'weight' in G[u][v]}
+nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=6, font_color='red', ax=ax)
+
+plt.title("Network Plot")
+plt.show()
